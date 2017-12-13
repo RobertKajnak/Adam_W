0c76d093e9b08c0471e65a105862dade7502a82c
robertkajnak <hezirisz@gmail.com>
2017-12-13 13:43:04 +0100

    completed chat skeleton

--- Adam/app/src/main/java/com/adam/adam/adam/ChatActivity.java
+++ Adam/app/src/main/java/com/adam/adam/adam/ChatActivity.java
@@ -2,27 +2,76 @@ package com.adam.adam.adam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.app.Activity;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ChatActivity extends AppCompatActivity {
 
-    TextView mMeassage;
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private Button buttonSend;
+    private boolean side = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        mMeassage = (TextView) findViewById(R.id.msg);
+        buttonSend = (Button) findViewById(R.id.send);
 
+        listView = (ListView) findViewById(R.id.msgview);
 
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.activity_right);
+        Log.d("ChatAct - chatarray",chatArrayAdapter==null?"Null":chatArrayAdapter.toString());
+        Log.d("ChatAct - listView",listView==null?"NUll":listView.toString());
+        Log.d("ChatAct - button",buttonSend==null?"NUll":buttonSend.toString());
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText = (EditText) findViewById(R.id.msg);
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return sendChatMessage();
+                }
+                return false;
+            }
+        });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                sendChatMessage();
+            }
+        });
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
     }
 
-    public void onSendButton(View view) {
-        mMeassage.setText("");
+    private boolean sendChatMessage() {
+        chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+        chatText.setText("");
+        side = !side;
+        return true;
     }
-
 }
+
+
---
+++ Adam/app/src/main/java/com/adam/adam/adam/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.adam.adam.adam;
+
+/**
+ * Created by Hesiris on 13/12/2017.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
