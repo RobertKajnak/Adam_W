0823f2e99cbe57f295b9ab1f7dcbf6332ded0471
robertkajnak <hezirisz@gmail.com>
2017-12-13 12:59:07 +0100

    added some chat section

--- Adam/app/src/main/java/com/adam/adam/adam/ChatActivity.java
+++ Adam/app/src/main/java/com/adam/adam/adam/ChatActivity.java
@@ -2,12 +2,27 @@ package com.adam.adam.adam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ChatActivity extends AppCompatActivity {
+
+    TextView mMeassage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mMeassage = (TextView) findViewById(R.id.msg);
+
+
     }
+
+    public void onSendButton(View view) {
+        mMeassage.setText("");
+    }
+
 }
